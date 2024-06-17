ff3fe0ca1bd15b6b7e15401098393326d4012ff6
renshuai <renfreedom@foxmail.com>
2024-06-17 14:44:15 +0800

    数据集制作

---
+++ datasets/README.txt
@@ -0,0 +1,12 @@
+1. 在datasets文件夹下创建新的数据集文件夹，比如sample
+2. 将此路径下的general_json2yolo.py, labelme2coco.py, labels.txt, mkd.sh, sample.yaml复制到上一步新建的文件夹sample
+3. 修改labels.txt，从第三行开始修改标签内容
+4. 修改sample.yaml中的path和names
+5. 上传zip格式的数据集压缩包到sample文件夹
+6. 执行./mkd.sh制作数据集
+
+mkd.sh脚本流程如下
+1. 创建temp, images, labels文件夹，其中temp用于解压数据集最终会删除，images路径下生成train文件夹包含所有图像，labels路径下生成train文件夹包含所有标签，如果文件夹存在会清空其中的所有文件
+2. 解压zip数据集至当前路径，然后移动所有图像到temp下，最后删除空文件夹
+3. labelme格式转为coco格式，生成annotations.json和train文件夹包含所有图像，将train移动到images下
+4. coco格式转成yolov8格式
---
+++ datasets/mkd.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# 目录名称
+dirs=("temp" "images" "labels")
+
+# 遍历目录名称数组
+for dir in "${dirs[@]}"; do
+  # 检查目录是否存在
+  if [ -d "$dir" ]; then
+    echo "路径 $dir 存在. 已删除..."
+    rm -rf "$dir"  # 删除目录及其内容
+  fi
+
+  # 创建新目录
+  echo "创建 $dir..."
+  mkdir "$dir"
+done
+
+# 目标文件夹路径
+destination="temp"
+
+# 遍历当前目录中的所有zip文件
+for file in *.zip; do
+    # 创建与zip文件同名的目录
+    unzip -q "$file"
+    
+    # 移动所有文件到目标文件夹
+    mv "${file%.zip}"/* "$destination"
+    
+    # 删除空文件夹
+    rmdir "${file%.zip}"
+done
+
+echo "解压缩、移动和删除空文件夹完成！"
+
+python3 labelme2coco.py $destination ./ --labels=labels.txt
+
+rm -r $destination
+mv train images
+
+python3 general_json2yolo.py
+
+#rm annotations.json
+
+echo "数据集制作完成."
